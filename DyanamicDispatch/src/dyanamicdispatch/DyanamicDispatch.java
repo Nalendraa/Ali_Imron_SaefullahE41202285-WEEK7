b93ca22c0cd7b5952892daa16318163446cefe66
Nalendraa <shawnaxcel42@gmail.com>
2021-04-24 15:00:27 +0700

    Create DyanamicDispatch.java

---
+++ DyanamicDispatch/src/dyanamicdispatch/DyanamicDispatch.java
@@ -0,0 +1,26 @@
+package dyanamicdispatch;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class DyanamicDispatch {
+
+    public static void main(String[] args) {
+        
+     A a = new A();
+     B b = new B();
+     C c = new C();
+     A ref;
+     
+     ref = b;
+     ref.callthis();
+     
+     ref = c;
+     ref.callthis();
+     
+     ref = a;
+     ref.callthis();
+    }
+    
+}
