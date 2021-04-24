eed242854635e3c5e3ec2ee02dfccfcf1e320c2d
Nalendraa <shawnaxcel42@gmail.com>
2021-04-24 15:00:44 +0700

    Create C.java

---
+++ DyanamicDispatch/src/dyanamicdispatch/C.java
@@ -0,0 +1,13 @@
+package dyanamicdispatch;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class C extends A
+{
+    void callthis ()
+    {
+         System.out.println("Inside Class C's Methode!");
+    }
+}
