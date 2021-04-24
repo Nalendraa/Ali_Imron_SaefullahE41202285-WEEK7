05dbb34d06db52d9b9a3955f5239a4c517a1515f
Nalendraa <shawnaxcel42@gmail.com>
2021-04-24 15:00:37 +0700

    Create B.java

---
+++ DyanamicDispatch/src/dyanamicdispatch/B.java
@@ -0,0 +1,13 @@
+package dyanamicdispatch;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class B extends A
+{
+    void callthis ()
+    {
+         System.out.println("Inside Class B's Methode!");
+    }
+}
